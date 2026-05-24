41c030a6d9198b0569c3e2bc798f3f3f9e92c1e5
segfaultuwu <284806546+segfaultuwu@users.noreply.github.com>
2026-05-24 19:45:17 +0200

    hope it works..

--- src/lib.rs
+++ src/lib.rs
@@ -10,24 +10,24 @@ pub const SYS_EXIT: u64 = 3;
 #[inline(always)]
 pub unsafe fn syscall0(num: u64) -> u64 {
     let ret: u64;
-
     unsafe {
         core::arch::asm!(
             "syscall",
             inlateout("rax") num => ret,
             lateout("rcx") _,
             lateout("r11") _,
+            lateout("r8")  _,
+            lateout("r9")  _,
+            lateout("r10") _,
             options(nostack),
         );
     }
-
     ret
 }
 
 #[inline(always)]
 pub unsafe fn syscall1(num: u64, arg1: u64) -> u64 {
     let ret: u64;
-
     unsafe {
         core::arch::asm!(
             "syscall",
@@ -35,17 +35,18 @@ pub unsafe fn syscall1(num: u64, arg1: u64) -> u64 {
             in("rdi") arg1,
             lateout("rcx") _,
             lateout("r11") _,
+            lateout("r8")  _,
+            lateout("r9")  _,
+            lateout("r10") _,
             options(nostack),
         );
     }
-
     ret
 }
 
 #[inline(always)]
 pub unsafe fn syscall2(num: u64, arg1: u64, arg2: u64) -> u64 {
     let ret: u64;
-
     unsafe {
         core::arch::asm!(
             "syscall",
@@ -54,20 +55,33 @@ pub unsafe fn syscall2(num: u64, arg1: u64, arg2: u64) -> u64 {
             in("rsi") arg2,
             lateout("rcx") _,
             lateout("r11") _,
+            lateout("r8")  _,
+            lateout("r9")  _,
+            lateout("r10") _,
             options(nostack),
         );
     }
-
     ret
 }
 
-pub fn write(bytes: &[u8]) -> u64 {
-    unsafe { syscall2(SYS_WRITE, bytes.as_ptr() as u64, bytes.len() as u64) }
+#[inline(always)]
+pub unsafe fn syscall1_noreturn(num: u64, arg1: u64) -> ! {
+    unsafe {
+        core::arch::asm!(
+            "syscall",
+            in("rax") num,
+            in("rdi") arg1,
+            options(nostack, noreturn),
+        );
+    }
+}
+
+pub fn write(bytes: &[u8]) {
+    unsafe { syscall2(SYS_WRITE, bytes.as_ptr() as u64, bytes.len() as u64) };
 }
 
 pub fn read_key() -> Option<u8> {
     let c = unsafe { syscall0(SYS_READ_KEY) };
-
     if c == 0 {
         None
     } else {
@@ -76,11 +90,7 @@ pub fn read_key() -> Option<u8> {
 }
 
 pub fn exit(code: u64) -> ! {
-    unsafe {
-        syscall1(SYS_EXIT, code);
-    }
-
-    loop {}
+    unsafe { syscall1_noreturn(SYS_EXIT, code) }
 }
 
 pub struct Writer;
@@ -94,7 +104,6 @@ impl core::fmt::Write for Writer {
 
 pub fn _print(args: core::fmt::Arguments) {
     use core::fmt::Write;
-
     let mut writer = Writer;
     let _ = writer.write_fmt(args);
 }
@@ -108,29 +117,16 @@ macro_rules! print {
 
 #[macro_export]
 macro_rules! println {
-    () => {{
-        $crate::print!("\n")
-    }};
-
-    ($fmt:expr) => {{
-        $crate::print!(concat!($fmt, "\n"))
-    }};
-
-    ($fmt:expr, $($arg:tt)*) => {{
-        $crate::print!(
-            concat!($fmt, "\n"),
-            $($arg)*
-        )
-    }};
+    () => { $crate::print!("\n") };
+    ($fmt:expr) => { $crate::print!(concat!($fmt, "\n")) };
+    ($fmt:expr, $($arg:tt)*) => {
+        $crate::print!(concat!($fmt, "\n"), $($arg)*)
+    };
 }
 
 #[cfg(feature = "panic-handler")]
-use core::panic::PanicInfo;
-
-#[cfg(feature = "panic-handler")]
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(_info: &core::panic::PanicInfo) -> ! {
     write(b"panic\n");
-
-    loop {}
+    exit(1);
 }
